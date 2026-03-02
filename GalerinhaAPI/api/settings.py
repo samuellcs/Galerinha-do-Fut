c491a7d6024dbc5e57d47974fa63b3f685d4db39
samuellcs <samuel.lucas7722@gmail.com>
2026-03-02 10:21:54 -0300

    fix: settings

--- GalerinhaAPI/api/settings.py
+++ GalerinhaAPI/api/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key-change-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
